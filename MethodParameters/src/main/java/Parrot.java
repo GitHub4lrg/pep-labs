19d3f78c3d3ae0e793070422a7229905880d51ec
Luis R Guerrero <lrgutube09@gmail.com>
2022-12-20 08:39:25 +0000

    Saved progress on MethodParameters 2022-12-20 08:39:25 +0000

--- MethodParameters/src/main/java/Parrot.java
+++ MethodParameters/src/main/java/Parrot.java
@@ -11,7 +11,10 @@ public class Parrot {
      * @param text is a parameter passed into this method by whatever called it.
      * @return text.
      */
-    public String copy(String text){
-        return "return text please!";
+    public String main(String hello){
+        return "hello";
+    }
+    public String copy(String polly){
+        return "polly";
     }
 }
