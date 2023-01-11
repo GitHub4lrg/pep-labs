72832c1a38825f3379e6ba7208d1bf36e760db56
Luis R Guerrero <lrgutube09@gmail.com>
2023-01-11 20:32:07 +0000

    Saved progress on MaxMinusMin 2023-01-11 20:32:07 +0000

--- MaxMinusMin/src/main/java/MaxMinusMin.java
+++ MaxMinusMin/src/main/java/MaxMinusMin.java
@@ -11,16 +11,14 @@ public class MaxMinusMin {
      * @return the max value of nums minus the min value of nums.
      */
     public int difference(List<Integer> nums){
-        List<Integer> l1 = new ArrayList<Integer>();
-
-        int max_val = nums[0];
-	    int min = nums[0];
-        for(int i = 1; i < nums.length; i++){
-            if(nums[i] > max_val)
-                max_val = nums[i];
-            else if(nums[i] < min)
-                min = nums[i];
-        return max_val-min;
+        int max_val = nums.get(0);
+	    int min = nums.get(0);
+        for(int i = 1; i < nums.size(); i++){
+            if(nums.get(i) > max_val)
+                max_val = nums.get(i);
+            else if(nums.get(i) < min)
+                min = nums.get(i);
         }
+        return max_val-min;
     }
 }
