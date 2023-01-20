7b34c7459590e4fd2bf35d36a20a948bb4fe36fd
Luis R Guerrero <lrgutube09@gmail.com>
2023-01-20 21:19:19 +0000

    Saved progress on IndexOf 2023-01-20 21:19:19 +0000

--- IndexOf/src/main/java/IndexOf.java
+++ IndexOf/src/main/java/IndexOf.java
@@ -1,3 +1,4 @@
+import java.util.stream.IntStream;
 
 //
 
@@ -11,6 +12,6 @@ public class IndexOf {
      * @return the first index (position) of n in arr. if n does not exist in arr, return -1.
      */
     public int getIndex(int[] arr, int n){
-        return -1;
+        return IntStream.range(0, arr.length).filter(i -> arr[i] == n).findFirst().orElse(-1);
     }
 }
