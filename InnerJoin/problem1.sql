50b0d0febf9c5075361a20b6e5996fd73ca6be84
Luis R Guerrero <lrgutube09@gmail.com>
2023-01-24 21:37:02 +0000

    Saved progress on InnerJoin 2023-01-24 21:37:02 +0000

--- InnerJoin/problem1.sql
+++ InnerJoin/problem1.sql
@@ -1,3 +1,3 @@
-SELECT student.id, student.student_name  FROM student 
-INNER JOIN class ON class.class_title = student.class_title 
-WHERE class.teacher_name = "Ms. Lovelace";
+SELECT student.id, student.student_name FROM class 
+INNER JOIN student ON class.class_title = student.class_title 
+WHERE class.teacher_name = 'Ms. Lovelace';
