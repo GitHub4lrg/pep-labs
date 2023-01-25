5e192de9ac8f8353bf1d502d9f6f8c9070ac1a09
Luis R Guerrero <lrgutube09@gmail.com>
2023-01-25 16:41:33 +0000

    Saved progress on ContainsDuplicates 2023-01-25 16:41:33 +0000

--- ContainsDuplicates/src/main/java/ContainsDuplicates.java
+++ ContainsDuplicates/src/main/java/ContainsDuplicates.java
@@ -1,4 +1,5 @@
-
+import java.util.HashSet;
+import java.util.Set;
 
 public class ContainsDuplicates {
     /**
@@ -8,6 +9,21 @@ public class ContainsDuplicates {
      * @return true if nums contains any duplicate values, false if it does not.
      */
     public boolean containsDuplicate(int[] nums){
+        //Set<Integer> has_duplicate = new HashSet<Integer>();
+        //for(int i =0; i < nums.length; i++){
+            //if(has_duplicate.contains(nums)){
+                //return true;
+            //}
+        //}
+        //return false;
+    //}
+//}
+        Set<Integer> has_duplicate = new HashSet<Integer>();
+        for (int i : nums){        // (int i =0; i < nums.length; i++) need to figure this solution instead
+            
+            if (has_duplicate.contains(i)) return true;
+            has_duplicate.add(i);
+        }
         return false;
     }
 }
