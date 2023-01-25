267401688bc127c030d4bb90f4580a383fa3feab
Luis R Guerrero <lrgutube09@gmail.com>
2023-01-25 18:08:35 +0000

    Saved progress on Search2dArray 2023-01-25 18:08:35 +0000

--- Search2dArray/src/main/java/Search2dArray.java
+++ Search2dArray/src/main/java/Search2dArray.java
@@ -10,6 +10,13 @@ public class Search2dArray {
      * @return true if nums contains target, and false otherwise.
      */
     public boolean search(int[][] nums, int target){
+        for(int rows = 0; rows < nums.length; rows++){
+            for(int columns = 0; columns < nums[rows].length; columns++){
+                if(nums[rows][columns] == target){
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
